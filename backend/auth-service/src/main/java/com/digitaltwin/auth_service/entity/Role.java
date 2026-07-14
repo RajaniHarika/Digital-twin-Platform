87f60ec8673182d8a1fb2f531f9f60c1bb913a70
RajaniHarika <harikarajani35@gmail.com>
2026-07-14 22:22:47 +0530

    Created auth-service project structure and entities

---
+++ backend/auth-service/src/main/java/com/digitaltwin/auth_service/entity/Role.java
@@ -0,0 +1,10 @@
+package com.digitaltwin.auth_service.entity;
+
+public enum Role {
+    ADMIN,
+    DEVOPS_ENGINEER,
+    CLOUD_ENGINEER,
+    BACKEND_ENGINEER,
+    PROJECT_MANAGER,
+    SRE_ENGINEER
+}
