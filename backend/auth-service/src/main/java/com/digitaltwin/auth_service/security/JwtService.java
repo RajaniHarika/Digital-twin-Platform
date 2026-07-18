ac36f192cfc09e30fcf1eae3c371db2f0833aff3
RajaniHarika <harikarajani35@gmail.com>
2026-07-18 22:47:57 +0530

    Complete Auth Service JWT authentication and add .gitignore

--- backend/auth-service/src/main/java/com/digitaltwin/auth_service/security/JwtService.java
+++ backend/auth-service/src/main/java/com/digitaltwin/auth_service/security/JwtService.java
@@ -3,19 +3,21 @@ package com.digitaltwin.auth_service.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "mySuperSecretKeyForDigitalTwinPlatformJwtAuthentication123456";
+    @Value("${jwt.secret}")
+    private String secret;
 
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(String email) {
